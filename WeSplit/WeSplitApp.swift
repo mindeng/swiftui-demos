8fd9ee128b8c4c3759e8f9c1964da4e15d24b727
Min Deng <minotes.net@gmail.com>
2023-09-26 17:54:59 +0800

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Min Deng on 2023/9/26.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
