63ff92e2a7787a8a62a3f754df4054b458664407
Min Deng <minotes.net@gmail.com>
2023-09-28 19:48:05 +0800

    add GuessTheFlag

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Min Deng on 2023/9/28.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
