d06f7c40d5bafc326dda9d615f1c44d0a10df07c
Min Deng <minotes.net@gmail.com>
2023-10-13 10:18:04 +0800

    add Animations

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Min Deng on 2023/10/10.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
